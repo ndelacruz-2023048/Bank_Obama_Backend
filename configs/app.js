1e5e57c2c06902366ce4716d43158e3b0dec4072
jgonzalez-2023195 <jgonzalez-2023195@kinal.edu.gt>
2025-06-09 15:32:54 -0600

    Se arregla un error

--- configs/app.js
+++ configs/app.js
@@ -11,7 +11,12 @@ import { limiter } from '../middlewares/rate.limit.js'
 const configs = (app)=> {
     app.use(express.json())
     app.use(express.urlencoded({extended: false}))
-    app.use(cors())
+    app.use(cors(
+        {
+            origin: 'http://localhost:5173',
+            credentials: true,
+        }
+    ))
     app.use(helmet())
     app.use(cookieParser())
     app.use(morgan('dev'))
